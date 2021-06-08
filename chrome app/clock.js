210406e01f154f0ad621c8b5a39c96086c43fdb7
Esther-jiwon <estheryun0807@gmail.com>
2021-06-08 21:18:40 +0900

    Bootstrap - Grid System

---
+++ chrome app/clock.js
@@ -0,0 +1,11 @@
+const clockContainer = document.querySelector(".js-clock");
+const clockTitle = clockContainer.querySelector("h1");
+
+function getTime() {
+    const date = new Date();
+
+}
+function init() {
+
+}
+
